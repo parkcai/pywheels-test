675e90aea54ad15abdc3c3b8241aee554d34e928
parkcai <162147799+parkcai@users.noreply.github.com>
2025-07-16 23:11:43 +0800

    update

--- task_runner-test/task_runner.py
+++ task_runner-test/task_runner.py
@@ -4,12 +4,13 @@ from pywheels.task_runner import execute_python_script
 def main():
     
     python_script = r"""
-print(1 + 1)
+print(1 + 1, end = "")
 """
 
     run_script_result = execute_python_script(python_script)
     
-    print(run_script_result["stdout"])
+    print(f"success: {run_script_result['success']}")
+    print(f"stdout: {run_script_result['stdout']}")
 
 
 if __name__ == "__main__":
