84e5ff0c1195752990e7d8994edec28ef82b387b
parkcai <162147799+parkcai@users.noreply.github.com>
2025-09-08 17:52:53 +0800

    adapt get_answer test script to new version pywheels

--- llm_tools-test/get_answer.py
+++ llm_tools-test/get_answer.py
@@ -3,16 +3,32 @@ from pywheels.llm_tools.get_answer import get_answer
 
 def main():
     
-    model_name = "Qwen_Plus"
-        
+    model = "Qwen_Plus"
+       
+    # test simple 
     prompt = input("[Me] ")
-    
     llm_answer = get_answer(
-        model_name = model_name,
+        model = model,
         prompt = prompt,
     )
+    print(f"[{model}] {llm_answer}")
     
-    print(f"[{model_name}] {llm_answer}")
+    # test elaborate
+    prompt = input(f"[Me, attaching a picture about a dog drinking water near a river]\n")
+    llm_answer = get_answer(
+        prompt = prompt,
+        model = model,
+        system_prompt = "Please answer the following question in an ironic way.",
+        images = [],
+        temperature = 1.0,
+        top_p = 0.5,
+        max_completion_tokens = 4096,
+        timeout = 300,
+        trial_num = 10,
+        trial_interval = 1,
+        check_and_accept = lambda _: True,
+    )
+    print(f"[{model}] {llm_answer}")
         
         
 if __name__ == "__main__":
